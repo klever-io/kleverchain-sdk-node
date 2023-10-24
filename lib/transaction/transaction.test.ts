d1173c9689d1a6309ac9ed34aa8490812ae11264
Samuel Façanha <samufacanha@gmail.com>
2023-10-24 09:47:39 -0300

    test: improved test case

--- lib/transaction/transaction.test.ts
+++ lib/transaction/transaction.test.ts
@@ -89,11 +89,42 @@ describe("Transaction", () => {
   });
 
   it("should be able to create a transaction from a decoded hex", async () => {
+    const expectedTx = {
+      RawData: {
+        Nonce: 161,
+        Sender: "YAqfGX0XLFJGddcsu6Eq/F+WKg8BmQ01IapVn2oJ5E0=",
+        Contract: [
+          {
+            Type: 0,
+            Parameter: {
+              type_url: "type.googleapis.com/proto.TransferContract",
+              value:
+                "CiBIXSEqNUEP3vcxQZ+TgKmimE2IU4ioB8KX08LLLEZ83hIDS0xWGIDC1y8=",
+            },
+          },
+        ],
+        PermissionID: 0,
+        Data: ["S0xFVkVSIFRFU1Q="],
+        KAppFee: 500000,
+        BandwidthFee: 1000000,
+        Version: 1,
+        ChainID: "MTAwNDIw",
+      },
+      Signature: [
+        "HifozgqUS9GbbROm1hnWvf9RJj9dh2F+ZXkWxUdtbfkzlXXq0KB5IwT9Q06R2iDmg3D/Qfjb2Dz0YHxnzqe1DA==",
+      ],
+      Result: 0,
+      ResultCode: 0,
+      Receipts: [],
+      Block: 0,
+    };
+
     const tx = new Transaction();
 
     tx.fromHex(
       "0aa30108a1011220600a9f197d172c524675d72cbba12afc5f962a0f01990d3521aa559f6a09e44d325c125a0a2a747970652e676f6f676c65617069732e636f6d2f70726f746f2e5472616e73666572436f6e7472616374122c0a20485d212a35410fdef731419f9380a9a2984d885388a807c297d3c2cb2c467cde12034b4c561880c2d72f520b4b4c45564552205445535468a0c21e70c0843d780182010631303034323012401e27e8ce0a944bd19b6d13a6d619d6bdff51263f5d87617e657916c5476d6df9339575ead0a0792304fd434e91da20e68370ff41f8dbd83cf4607c67cea7b50c"
     );
-    console.log(tx.toJSON());
+
+    expect(tx.toJSON()).toEqual(JSON.stringify(expectedTx));
   });
 });
