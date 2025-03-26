31da422e99a312bb32aa78abc5cd27d91535a4af
Samuel Façanha <samufacanha@gmail.com>
2025-03-26 17:33:25 -0300

    test: build tx options

--- lib/account/account.test.ts
+++ lib/account/account.test.ts
@@ -44,4 +44,36 @@ describe("Account", () => {
 
     expect(res1?.RawData).toBeTruthy();
   });
+
+  it("should be able to build a transaction with kda fee", async () => {
+    utils.setProviders({
+      api: "https://api.testnet.klever.finance",
+      node: "https://node.testnet.klever.finance",
+    });
+
+    const privateKey = process.env.PRIVATE_KEY;
+
+    const transferPayload = {
+      amount: 1 * 10 ** 6,
+      receiver:
+        "klv1nnu8d0mcqnxunqyy5tc7kj7vqtp4auy4a24gv35fn58n2qytl9xsx7wsjl",
+      kda: "KLV",
+    };
+
+    const tx = {
+      type: TransactionType.Transfer,
+      payload: transferPayload,
+    };
+
+    const account = new Account(privateKey);
+    await utils.accountsReady([account]);
+
+    const options = {
+      kdaFee: "KLV",
+    };
+
+    const res1 = await account.buildTransaction([tx], undefined, options);
+
+    expect(res1?.RawData).toBeTruthy();
+  });
 });
